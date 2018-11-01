d9cd2583decf503287130d0ad821cfb950be75db
mobidiic <sigus128@gmail.com>
2018-11-01 12:48:18 +0900

    fit Comments component to reducer

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -14,11 +14,20 @@ class Comments extends Component{
         )
     }
 
+    handleSubmit(e) {
+        e.preventDefault();
+        const { postId } = this.props.params;
+        const author = this.refs.author.value;
+        const comment = this.refs.comment.value;
+        this.props.addComment(postId, author, comment);
+        this.refs.commentForm.reset();
+      }
+
     render(){
         return(
             <div className="comments">
                 {this.props.postComments.map(this.renderComment)}
-                <form ref="commentForm" className="comment-form">
+                <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit}>
                     <input type="text" ref="author" placeholder="author"></input>
                     <input type="text" ref="comment" placeholder="comment"></input>
                     <input type="submit" hidden />
--- src/components/Single/Single.js
+++ src/components/Single/Single.js
@@ -10,33 +10,35 @@ class Single extends Component{
     // const i = this.props.posts.findIndex( (post) => post.code === postId)
     // const post = this.props.posts[i]
     // const postComments = this.props.comments[postId] || []
+    const testPost = {
+      "code": "BAcJeJrQca9",
+      "caption": "Snow! ⛄️🌨❄️ #lifewithsnickers",
+      "likes": 59,
+      "id": "1160844458347054781",
+      "display_src": `https://picsum.photos/400/400/?image=${Math.floor((Math.random() * 85))}`,
+    }
+    const testComment = [
+      {
+        "text":"Wes. WE should have lunch.",
+        "user": "jdaveknox"
+      },
+      {
+        "text":"#adults",
+        "user": "jdaveknox"
+      },
+      {
+        "text":"@jdaveknox yes!",
+        "user": "wesbos"
+      },
+      {
+        "text":"😍 love Hamilton!",
+        "user": "willowtreemegs"
+      }
+    ]
     return(
       <div className="single-photo">
-        <Photo i={"BAcJeJrQca9"} post={ {
-    "code": "BAcJeJrQca9",
-    "caption": "Snow! ⛄️🌨❄️ #lifewithsnickers",
-    "likes": 59,
-    "id": "1160844458347054781",
-    "display_src": `https://picsum.photos/400/400/?image=${Math.floor((Math.random() * 85))}`,
-  }} {...this.props} />
-        <Comments postComments={[{"BAcyDyQwcXX":[
-    {
-      "text":"Wes. WE should have lunch.",
-      "user": "jdaveknox"
-    },
-    {
-      "text":"#adults",
-      "user": "jdaveknox"
-    },
-    {
-      "text":"@jdaveknox yes!",
-      "user": "wesbos"
-    },
-    {
-      "text":"😍 love Hamilton!",
-      "user": "willowtreemegs"
-    }
-  ]}]} />
+        <Photo i={"BAcJeJrQca9"} post={testPost} {...this.props} />
+        <Comments postComments={testComment} />
       </div>
     )
   }
